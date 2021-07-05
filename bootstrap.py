e455f5d64696bfd66142a6169356ba2fcbcb8dac
Rusty Russell <rusty@rustcorp.com.au>
2021-07-05 21:07:47 +0930

    Add mainnet examples.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- bootstrap.py
+++ bootstrap.py
@@ -137,6 +137,32 @@ def example_page():
             { "offer": "2559740fb28cc23982557fa6e74ad7203a829eeb8538fed2cd39431de5b073d7",
               "bolt12": "lno1qgsqvgnwgcg35z6ee2h3yczraddm72xrfua9uve2rlrm9deu7xyfzrcxqd24x3qgqgp7szs0xyc9256yypjhvetj0ysxgctezsg8yatnw3ujumm6d3skyuewdaexwxszqyq3ugxff8l7qsrc3s9szhc62zjrljhgg0ssuq56fwsgxql3dq99609xa8cyqcydgst24m87jtlu3v4nwxkxr4dg9aymaxjgxwsr9mspfsayqmuaf9lz2gm6wf3clx0wj4ekyu0epu5du5lxl4xxaf6ygdm9mtpk27ms",
               "bolt12_unsigned": "lno1qgsqvgnwgcg35z6ee2h3yczraddm72xrfua9uve2rlrm9deu7xyfzrcxqd24x3qgqgp7szs0xyc9256yypjhvetj0ysxgctezsg8yatnw3ujumm6d3skyuewdaexwxszqyq3ugxff8l7qsrc3s9szhc62zjrljhgg0ssuq56fwsgxql3dq99609xay" }]
+    elif network == 'bitcoin':
+        examples=[
+            { "offer": "28522b52ac39fa518ce3a5b3e4a9a96372487e78ba5eb1540ec4d9f02ca82718",
+              "bolt12": "lno1pg257enxv4ezqcneype82um50ynhxgrwdajx283qfwdpl28qqmc78ymlvhmxcsywdk5wrjnj36jryg488qwlrnzyjczlqs85ck65ycmkdk92smwt9zuewdzfe7v4aavvaz5kgv9mkk63v3s0ge0f099kssh3yc95qztx504hu92hnx8ctzhtt08pgk0texz0509tk",
+              "bolt12_unsigned": "lno1pg257enxv4ezqcneype82um50ynhxgrwdajx283qfwdpl28qqmc78ymlvhmxcsywdk5wrjnj36jryg488qwlrnzyjczs" },
+            { "offer": "1cacc1f7d2b3a6b5f698069e3793f96bdcc32cfb249dde3b1d55c7f2f85c2985",
+              "bolt12": "lno1pqqnyzsmx5cx6umpwssx6atvw35j6ut4v9h8g6t50ysx7enxv4epgrmjw4ehgcm0wfczucm0d5hxzagkqyq3ugztng063cqx783exlm97ekyprnd4rsu5u5w5sez9fecrhcuc3ykqhcypjju7unu05vav8yvhn27lztf46k9gqlga8uvu4uq62kpuywnu6me8srgh2q7puczukr8arectaapfl5d4rd6uc7st7tnqf0ttx39n40s",
+              "bolt12_unsigned": "lno1pqqnyzsmx5cx6umpwssx6atvw35j6ut4v9h8g6t50ysx7enxv4epgrmjw4ehgcm0wfczucm0d5hxzagkqyq3ugztng063cqx783exlm97ekyprnd4rsu5u5w5sez9fecrhcuc3ykq5" },
+            { "offer": "34145f989a97260e6e1e1fb835ea4e58f582d514cc3e4e91dcdde114d2130578",
+              "bolt12": "lno1pqqkgzs5xycrqmtnv96zqetkv4e8jgrdd9h82ar9zsg8yatnw3ujumm6d3skyuewdaexwxszqq7pugztng063cqx783exlm97ekyprnd4rsu5u5w5sez9fecrhcuc3ykqhcyp5jq3tl57jh3g32qwfy9hpa88eqwst78gn06v9xcd485hezgs8xe2800mrth3tznd5884dnnpg3sd0tvv7k9er8ex5pg0prrnl66xd7s",
+              "bolt12_unsigned": "lno1pqqkgzs5xycrqmtnv96zqetkv4e8jgrdd9h82ar9zsg8yatnw3ujumm6d3skyuewdaexwxszqq7pugztng063cqx783exlm97ekyprnd4rsu5u5w5sez9fecrhcuc3ykq5" },
+            { "offer": "c1a4b907e4a95fe72c3d561f2ad33e96cbb509ca50f9fee1338f15949f3250bf",
+              "bolt12": "lno1pqqkgz38xycrqmtnv96zqetkv4e8jgrdd9h82ar99ss82upqw3hjqargwfjk2gr5d9kk2uc5zpe82um50yhx77nvv938xtn0wfn35qsq8s0zqju6r75wqph3uwfh7e0kd3qgumdgu8989r4yxg32wwqa78xyf9s9ggqs9uzqd7zy5v0cehj4ev52g7ljaec4qqdmjk2lkv58z32mn5z5q8rsyp6fpqnyu2q6gcz6nvmdtmtgyzs3xxhchr09qsupd4tngtq6frkpumc",
+              "bolt12_unsigned": "lno1pqqkgz38xycrqmtnv96zqetkv4e8jgrdd9h82ar99ss82upqw3hjqargwfjk2gr5d9kk2uc5zpe82um50yhx77nvv938xtn0wfn35qsq8s0zqju6r75wqph3uwfh7e0kd3qgumdgu8989r4yxg32wwqa78xyf9s9ggqsy" },
+            { "offer": "bf3c613604bb741ab649f0712fa26ec8c74f06aea159714989de2095e0737f85",
+              "bolt12": "lno1pqqkgz3zxycrqmtnv96zqetkv4e8jgryv9ujcgrxwfhk6gp3949xzm3dxgcryvg5zpe82um50yhx77nvv938xtn0wfn35qspqywq2q2laenqq83qfwdpl28qqmc78ymlvhmxcsywdk5wrjnj36jryg488qwlrnzyjczlqsytf0g06zjgcrtk0n09n5wk78ssdhckpmfqmfvlxm92u36egsmf3kswfpqt70dq6mg4lw3t8qx7feh6c8hxz2vwzsdg4n957z8gh8unx",
+              "bolt12_unsigned": "lno1pqqkgz3zxycrqmtnv96zqetkv4e8jgryv9ujcgrxwfhk6gp3949xzm3dxgcryvg5zpe82um50yhx77nvv938xtn0wfn35qspqywq2q2laenqq83qfwdpl28qqmc78ymlvhmxcsywdk5wrjnj36jryg488qwlrnzyjczs" },
+            { "offer": "37fec3bf68b1938c4f27f586edec66e9eff4e0c8fe1c752512b9e89a804fbc9a",
+              "bolt12": "lno1pqpq86q2fgcnqvpsd4ekzapqv4mx2uneyqcnqgryv9uhxtpqveex7mfqxyk55ctw95erqv339ss8qcteyqcksu3qvfjkvmmjv5s8gmeqxcczqum9vdhkuernypkxzar9zsg8yatnw3ujumm6d3skyuewdaexwxszqy9pcpgptlhxvqq7yp9e58aguqr0rcun0ajlvmzq3ek63cw2w282gv3z5uupmuwvgjtq2sqxqqqquyqq8ncyph3h0xskvfd69nppz2py2nxym7rlq255z4mevv5x7vqh077n792e3gcua5p734l7d2r0x7kat69gx6c3twqexgmplmmjz2tv9hne4j5s",
+              "bolt12_unsigned": "lno1pqpq86q2fgcnqvpsd4ekzapqv4mx2uneyqcnqgryv9uhxtpqveex7mfqxyk55ctw95erqv339ss8qcteyqcksu3qvfjkvmmjv5s8gmeqxcczqum9vdhkuernypkxzar9zsg8yatnw3ujumm6d3skyuewdaexwxszqy9pcpgptlhxvqq7yp9e58aguqr0rcun0ajlvmzq3ek63cw2w282gv3z5uupmuwvgjtq2sqxqqqquyqq8s" },
+            { "offer": "e66d7beb93ef1b1084a73e5de6975ebc75c20d1a177bd2fb022b110226baf716",
+              "bolt12": "lno1pqpq86q2xycnqvpsd4ekzapqv4mx2uneyqcnqgryv9uhxtpqveex7mfqxyk55ctw95erqv339ss8qun094exzarpzsg8yatnw3ujumm6d3skyuewdaexwxszqy9pcpgptlhxvqq7yp9e58aguqr0rcun0ajlvmzq3ek63cw2w282gv3z5uupmuwvgjtq2sqgqqxj7qqpp5hspuzq0pgmhkcg6tqeclvexaawhylurq90ezqrdcm7gapzvcyfzexkt8nmu628dxr375yjvax3x20cxyty8fg8wrr2dlq3nx45phn2kqru2cg",
+              "bolt12_unsigned": "lno1pqpq86q2xycnqvpsd4ekzapqv4mx2uneyqcnqgryv9uhxtpqveex7mfqxyk55ctw95erqv339ss8qun094exzarpzsg8yatnw3ujumm6d3skyuewdaexwxszqy9pcpgptlhxvqq7yp9e58aguqr0rcun0ajlvmzq3ek63cw2w282gv3z5uupmuwvgjtq2sqgqqxj7qqpp5hsq" },
+            { "offer": "7cef68df49fd9222bed0138ca5603da06464b2f523ea773bc4edcb6bd07966e7",
+              "bolt12": "lno1qcp4256ypqpq86q2pucnq42ngssx2an9wfujqerp0y2pqun4wd68jtn00fkxzcnn9ehhyec6qgqsz83qfwdpl28qqmc78ymlvhmxcsywdk5wrjnj36jryg488qwlrnzyjczlqsp9nyu4phcg6dqhlhzgxagfu7zh3d9re0sqp9ts2yfugvnnm9gxkcnnnkdpa084a6t520h5zhkxsdnghvpukvd43lastpwuh73k29qsy",
+              "bolt12_unsigned": "lno1qcp4256ypqpq86q2pucnq42ngssx2an9wfujqerp0y2pqun4wd68jtn00fkxzcnn9ehhyec6qgqsz83qfwdpl28qqmc78ymlvhmxcsywdk5wrjnj36jryg488qwlrnzyjczs" }]
 
         DATE_JAN1_2021 = 1609421400
         DAYS_SINCE = int((time.time() - DATE_JAN1_2021) // (60 * 60 * 24))
