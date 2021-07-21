a019c41c07bdf5d46aab269698a91e0bd2c2252a
Rusty Russell <rusty@rustcorp.com.au>
2021-07-21 16:46:55 +0930

    Commit configuration files too
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ etc/dehydrated/config.sh
@@ -0,0 +1,8 @@
+# deydrated (nee letsencrypt.sh) configuration
+CA="https://acme-v02.api.letsencrypt.org/directory"
+OLDCA="https://acme-v01.api.letsencrypt.org/directory"
+WELLKNOWN="${HOME}/etc/dehydrated/acme-challenges"
+CONTACT_EMAIL="rusty@rustcorp.com.au"
+LOCKFILE="${HOME}/etc/dehydrated/lock"
+PRIVATE_KEY_RENEW="no"
+RENEW_DAYS="30"
